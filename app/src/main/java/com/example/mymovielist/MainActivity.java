3745f4bd6be7c7235620007f4a57e448424d5272
MohdIssa <21001078@myrp.edu.sg>
2022-08-15 11:06:44 +0800

    Update with L12 requirements

--- app/src/main/java/com/example/mymovielist/MainActivity.java
+++ app/src/main/java/com/example/mymovielist/MainActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.content.Intent;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 
@@ -16,35 +20,100 @@ public class MainActivity extends AppCompatActivity {
     Button Add, List;
     EditText Title, Year, Genre, Rating;
     ArrayList<Movie> AL;
+    Spinner spin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        spin = findViewById(R.id.sp1);
         Add =findViewById(R.id.addBut);
         List =findViewById(R.id.listBut);
         Title =findViewById(R.id.tinp1);
         Genre =findViewById(R.id.tinp2);
         Year =findViewById(R.id.tinum);
-        Rating =findViewById(R.id.tinp3);
         AL = new ArrayList<>();
+
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.ratings_array, android.R.layout.simple_spinner_item);
+
+
+
+
 
         Add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String T = Title.getText().toString().trim();
-                String G = Genre.getText().toString().trim();
-                int Y = Integer.parseInt(Year.getText().toString());
-                String R = Rating.getText().toString().trim();
+                int bound = 0;
 
-                DBhelp db = new DBhelp(MainActivity.this);
-                long id = db.insertMovie(T,G,Y,R);
 
-                Title.setText("");
-                Genre.setText("");
-                Year.setText("");
-                Rating.setText("");
+                spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        String V;
+                        switch (i) {
+                            case 0:
+                                V = "g";
+                                break;
+                            case 1:
+                                V = "m18";
+                                break;
+                            case 2:
+                                V = "nc16";
+                                break;
+                            case 3:
+                                V = "pg";
+                                break;
+                            case 4:
+                                V = "pg13";
+                                break;
+                            case 5:
+                                V = "r21";
+                                break;
+                        }
+
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+
+                    }
+                });
+
+                String R = spin.getSelectedItem().toString();
+
+                if (TextUtils.isEmpty(Title.getText())) {
+                    Title.setError("Title name is empty");
+
+                    } else if (TextUtils.isEmpty(Genre.getText())) {
+                        Genre.setError("Genre name is empty");
+
+                    } else if (TextUtils.isEmpty(Year.getText())) {
+                        Year.setError("Year is empty");
+
+                    } else if (Integer.parseInt(Year.getText().toString()) > 2024 || Integer.parseInt(Year.getText().toString()) < 1950 ) {
+                        Year.setError("Year set is outside year boundary");
+                        bound = 1;
+
+                    } else if (!TextUtils.isEmpty(Title.getText()) && !TextUtils.isEmpty(Genre.getText()) && (!TextUtils.isEmpty(Year.getText()) && bound != 1 )) {
+
+                        String T = Title.getText().toString().trim();
+                        String G = Genre.getText().toString().trim();
+                        int Y = Integer.parseInt(Year.getText().toString());
+
+                        DBhelp db = new DBhelp(MainActivity.this);
+                        long id = db.insertMovie(T,G,Y,R);
+
+
+
+                    Title.setText("");
+                    Genre.setText("");
+                    Year.setText("");
+
+                }
 
             }
         });
--- app/src/main/java/com/example/mymovielist/MainActivityEdit.java
+++ app/src/main/java/com/example/mymovielist/MainActivityEdit.java
@@ -1,11 +1,17 @@
 package com.example.mymovielist;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
@@ -16,6 +22,7 @@ public class MainActivityEdit extends AppCompatActivity {
     EditText Title, Year, Genre, Rating;
     ArrayList<Movie> AL;
     Movie object;
+    Spinner spin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +34,9 @@ public class MainActivityEdit extends AppCompatActivity {
         Cancel = findViewById(R.id.Cancel);
         Title = findViewById(R.id.tinp1);
         Genre = findViewById(R.id.tinp2);
-        Rating = findViewById(R.id.tinp3);
         Year = findViewById(R.id.tinum);
         AL = new ArrayList<>();
+        spin = findViewById(R.id.Sp2);
 
         Intent i = getIntent();
         String data = (String) i.getSerializableExtra("AAA");
@@ -47,36 +54,152 @@ public class MainActivityEdit extends AppCompatActivity {
         Title.setText(object.getTitle());
         Genre.setText(object.getGenre());
         Year.setText(String.valueOf(object.getYear()));
-        Rating.setText(object.getRatings());
+       // Rating.setText(object.getRatings());
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.ratings_array, android.R.layout.simple_spinner_item);
+
+        spin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String V;
+                switch (i) {
+                    case 0:
+                        V = "g";
+                        break;
+                    case 1:
+                        V = "m18";
+                        break;
+                    case 2:
+                        V = "nc16";
+                        break;
+                    case 3:
+                        V = "pg";
+                        break;
+                    case 4:
+                        V = "pg13";
+                        break;
+                    case 5:
+                        V = "r21";
+                        break;
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+        spin.setSelection(adapter.getPosition(object.getRatings()));
 
         Edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                int bound = 0;
 
-                DBhelp dbh = new DBhelp(MainActivityEdit.this);
-                object.setTitle(Title.getText().toString());
-                object.setGenre(Genre.getText().toString());
-                object.setYear(Integer.valueOf(Year.getText().toString()));
-                object.setRatings(Rating.getText().toString());
-                dbh.updateMovie(object);
-                dbh.close();
-                finish();
+                if (TextUtils.isEmpty(Title.getText())) {
+                    Title.setError("Title name is empty");
+
+                } else if (TextUtils.isEmpty(Genre.getText())) {
+                    Genre.setError("Genre name is empty");
+
+                } else if (TextUtils.isEmpty(Year.getText())) {
+                    Year.setError("Year is empty");
+
+                } else if (Integer.parseInt(Year.getText().toString()) > 2024 || Integer.parseInt(Year.getText().toString()) < 1950) {
+                    Year.setError("Year set is outside year boundary");
+                    bound = 1;
+
+                } else if (!TextUtils.isEmpty(Title.getText()) && !TextUtils.isEmpty(Genre.getText()) && (!TextUtils.isEmpty(Year.getText()) && bound != 1)) {
+
+
+
+                    AlertDialog.Builder ABuilder = new AlertDialog.Builder(MainActivityEdit.this);
+
+                    ABuilder.setTitle("Apply Changes?");
+                    ABuilder.setMessage("Are you sure you want to apply these cahnges?");
+                    ABuilder.setCancelable(false);
+
+                    ABuilder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            AlertDialog mydialog = ABuilder.create();
+                            mydialog.show();
+
+                            DBhelp dbh = new DBhelp(MainActivityEdit.this);
+                            object.setTitle(Title.getText().toString());
+                            object.setGenre(Genre.getText().toString());
+                            object.setYear(Integer.valueOf(Year.getText().toString()));
+                            object.setRatings(spin.getSelectedItem().toString().trim());
+                            dbh.updateMovie(object);
+                            dbh.close();
+                            finish();
+                        }
+                    });
+                    ABuilder.setNegativeButton("CANCEL", null);
+
+                    AlertDialog mydialog = ABuilder.create();
+                    mydialog.show();
+
+                }
             }
         });
 
         Delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DBhelp dbh = new DBhelp(MainActivityEdit.this);
-                dbh.deleteMovie(object.getId());
-                finish();
+
+                AlertDialog.Builder ABuilder = new AlertDialog.Builder(MainActivityEdit.this);
+
+                ABuilder.setTitle("DELETE Movie");
+                ABuilder.setMessage("Are you sure you want to delete the movie \n" + object.getTitle().toString());
+                ABuilder.setCancelable(false);
+
+                ABuilder.setPositiveButton("DELETE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        DBhelp dbh = new DBhelp(MainActivityEdit.this);
+                        dbh.deleteMovie(object.getId());
+                        finish();
+                    }
+                });
+                ABuilder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+
+                AlertDialog mydialog = ABuilder.create();
+                mydialog.show();
+
+
             }
         });
 
         Cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+
+                AlertDialog.Builder ABuilder = new AlertDialog.Builder(MainActivityEdit.this);
+
+                ABuilder.setTitle("Cancel Changes?");
+                ABuilder.setMessage("Are you sure you want to discard changes?");
+                ABuilder.setCancelable(false);
+
+                ABuilder.setPositiveButton("DISCARD", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+                ABuilder.setNegativeButton("CANCEL", null);
+
+                AlertDialog mydialog = ABuilder.create();
+                mydialog.show();
             }
         });
 
